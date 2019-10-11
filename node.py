a94b1be0465e2843dbc965c13892d8fe6625b6f1
Masroor Hamdani <masroorhamdani@yahoo.in>
2019-10-11 14:28:35 +0800

    Algorithm for Adding element and removing and finding elements

--- node.py
+++ node.py
@@ -99,6 +99,7 @@ class LinkedList(object):
             slow_ptr = slow_ptr.next
             fast_ptr = fast_ptr.next.next
         print("Middle point:", slow_ptr.data)
+        return slow_ptr
 
     def find_mid_element_using_counter(self):
         mid_ptr = self.head
@@ -111,6 +112,7 @@ class LinkedList(object):
             curr = curr.next
         if mid_ptr != None:
             print("Middle point:", mid_ptr.data)
+            return mid_ptr
 
     def get_list_count(self):
         curr = self.head
@@ -145,6 +147,54 @@ class LinkedList(object):
                 return curr.data
             count+=1
             curr = curr.next
+
+    def replace_head_by_mid(self):
+        curr = self.head
+        mid_ptr = self.find_mid_element()
+        temp = curr.data
+        curr.data = mid_ptr.data
+        mid_ptr.data = temp
+
+    def insert_middle(self, data):
+        new_node = Node(data)
+        if (self.head == None):
+            self.head = new_node
+        else:
+            ptr = self.head
+            length = 0
+            while (ptr != None):
+                ptr = ptr.next
+                length += 1
+            if(length%2 == 0):
+                count = length/2
+            else:
+                count = (length + 1) /2
+            ptr = self.head
+            while (count >1):
+                ptr = ptr.next
+                count -= 1
+
+            new_node.next = ptr.next
+            ptr.next = new_node
+
+    def insert_middle_double_ptr(self, data):
+        new_node = Node(data)
+        mid_ptr = self.find_mid_element()
+        new_node.next = mid_ptr.next
+        mid_ptr.next = new_node
+
+    def insert_at_position(self, pos, data):
+        if pos < 1:
+            print ('Pass valid position')
+        else:
+            new_node = Node(data)
+            curr = self.head
+            count = 1
+            while curr and count < pos-1:
+                curr = curr.next
+                count += 1
+            new_node.next = curr.next
+            curr.next = new_node
 
 if __name__=='__main__': 
     # Start with the empty list 
@@ -206,8 +256,15 @@ if __name__=='__main__':
     llist3.print_list()
     print('1 to left of mid', llist3.get_kth_from_mid_to_left(1))
     print('2 to left of mid', llist3.get_kth_from_mid_to_left(2))
-    print('1 to left of mid', llist3.get_kth_from_mid_to_right(1))
-    print('2 to left of mid', llist3.get_kth_from_mid_to_right(2))
+    print('1 to right of mid', llist3.get_kth_from_mid_to_right(1))
+    print('2 to right of mid', llist3.get_kth_from_mid_to_right(2))
+
+    print('**** Replace Head by Middle node****')
+    llist3.print_list()
+    llist3.replace_head_by_mid()
+    print('*** After Replacement***')
+    llist3.print_list()
+
 
     print('*** Delete without Header pointer***')
     llist3.print_list()
@@ -215,3 +272,51 @@ if __name__=='__main__':
     llist3.delete_with_location_pointer(del_node)
     print('*** After Deletion***')
     llist3.print_list()
+
+    print('**** Replace Head by Middle node****')
+    llist3.print_list()
+    llist3.replace_head_by_mid()
+    print('*** After Replacement***')
+    llist3.print_list()
+
+    print('****Insert node in middle - Odd List****')
+    llist4 = LinkedList()
+    llist4.append_on_end(5)
+    llist4.append_on_end(10)
+    llist4.append_on_end(4)
+    llist4.append_on_end(32)
+    llist4.append_on_end(16)
+    
+    llist4.print_list()
+    llist4.insert_middle(41)
+    print('*** After Insert node in middle***')
+    llist4.print_list()
+
+    print('****Insert node in middle - Even List****')
+    llist5 = LinkedList()
+    llist5.append_on_end(1)
+    llist5.append_on_end(2)
+    llist5.append_on_end(4)
+    llist5.append_on_end(5)
+    llist5.print_list()
+    # llist5.insert_middle(3)
+    llist5.insert_middle_double_ptr(3)
+    print('*** After Insert node in middle after node len/2,***')
+    llist5.print_list()
+
+    print('****Insert node in middle -Using double pointer****')
+    llist6 = LinkedList()
+    llist6.append_on_end(1)
+    llist6.append_on_end(2)
+    llist6.append_on_end(4)
+    llist6.append_on_end(5)
+    llist6.print_list()
+    llist6.insert_middle_double_ptr(3)
+    print('*** After Insert node in middle,***')
+    llist6.print_list()
+
+    print('***Insert element at given position***')
+    llist6.print_list()
+    llist6.insert_at_position(3, 9)
+    print('***After Insert element at position 3, element 9***')
+    llist6.print_list()
