634c29d797cbade7e645e906d2537911fa51306a
Masroor Hamdani <masroorhamdani@yahoo.in>
2019-11-01 19:10:08 +0800

    Quick Sort algorithm

--- sort.py
+++ sort.py
@@ -63,6 +63,27 @@ def merge_sort(arr):
             j+=1
             k+=1
 
+def quick_sort(arr, low, high):
+    if(low < high):
+        pi = partition(arr, low, high)
+        quick_sort(arr, low, pi-1)
+        quick_sort(arr, pi+1, high)
+
+def partition(arr, low, high):
+    i= (low-1)
+    pivot = arr[high]
+
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i+=1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return (i + 1)
+
+def heap_sort():
+    pass
+
 if __name__=='__main__':
     arr = [64, 25, 12, 22, 11]
     print ("****** Selection Sort *******")
@@ -91,5 +112,13 @@ if __name__=='__main__':
     print ("UnSorted array"),
     print_array(arr)
     merge_sort(arr)
+    print ("Sorted array"),
+    print_array(arr)
+
+    arr = [10, 7, 8, 9, 1, 5]#[10, 80, 30, 90, 40, 50, 70]
+    print ("****** Quick Sort *******")
+    print ("UnSorted array"),
+    print_array(arr)
+    quick_sort(arr, 0, (len(arr) -1))
     print ("Sorted array"),
     print_array(arr)
